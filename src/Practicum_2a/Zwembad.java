373dd40499fa55c80a1eb0e1fff798dd4667718c
JunZhou04 <marcozhou277@gmail.com>
2025-11-28 23:31:36 +0100

    UML

--- src/Practicum_2a/Zwembad.java
+++ src/Practicum_2a/Zwembad.java
@@ -1,21 +1,20 @@
 package Practicum_2a;
 
 public class Zwembad {
-    // Attributen
+
     private double breedte;
     private double lengte;
     private double diepte;
 
-    // Constructor met parameters
     public Zwembad(double breedte, double lengte, double diepte) {
         this.breedte = breedte;
         this.lengte = lengte;
         this.diepte = diepte;
     }
 
-    // Lege constructor
+
     public Zwembad() {
-        // Waarden blijven standaard 0.0
+
     }
 
     // Getters
@@ -44,15 +43,14 @@ public class Zwembad {
         this.diepte = diepte;
     }
 
-    // Methode om inhoud te berekenen
+
     public double inhoud() {
         return breedte * lengte * diepte;
     }
 
-    // toString voor nette tekst
+
     @Override
     public String toString() {
-        return "Dit zwembad is " + breedte + " meter breed, "
-                + lengte + " meter lang, en " + diepte + " meter diep";
+        return "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
     }
 }
