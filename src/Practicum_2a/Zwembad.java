03b959a105199034101afd0b78183dcd415e71fa
JunZhou04 <marcozhou277@gmail.com>
2025-11-28 22:35:33 +0100

    all

--- src/Practicum_2a/Zwembad.java
+++ src/Practicum_2a/Zwembad.java
@@ -1,20 +1,21 @@
 package Practicum_2a;
 
 public class Zwembad {
-
+    // Attributen
     private double breedte;
     private double lengte;
     private double diepte;
 
-
+    // Constructor met parameters
     public Zwembad(double breedte, double lengte, double diepte) {
         this.breedte = breedte;
         this.lengte = lengte;
         this.diepte = diepte;
     }
 
+    // Lege constructor
     public Zwembad() {
-
+        // Waarden blijven standaard 0.0
     }
 
     // Getters
@@ -43,11 +44,12 @@ public class Zwembad {
         this.diepte = diepte;
     }
 
+    // Methode om inhoud te berekenen
     public double inhoud() {
         return breedte * lengte * diepte;
     }
 
-
+    // toString voor nette tekst
     @Override
     public String toString() {
         return "Dit zwembad is " + breedte + " meter breed, "
